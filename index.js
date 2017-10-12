4fcc3cd5d490bf76ceb24b666925cbc0093aecce
tomtrasmontero <tomtrasmontero@gmail.com>
2017-10-11 20:21:32 -0400

    added cors for cross origin request

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup
 mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 app.use(morgan('combined'));
+// all request will go through cors to handle cross origin resources sharing
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*'}));
 router(app);
 
